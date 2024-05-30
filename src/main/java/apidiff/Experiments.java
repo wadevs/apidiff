a7380afbc27925183ae38284b918c5b84850c3de
Sébastien Anthamatten <>
2024-05-30 02:38:19 +0200

    Comments cleanup

--- src/main/java/apidiff/Experiments.java
+++ src/main/java/apidiff/Experiments.java
@@ -5,19 +5,14 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-// import java.util.Date;
 import java.util.List;
 import java.util.Map;
-
-import javax.print.DocFlavor.STRING;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
-import org.eclipse.jgit.diff.Edit;
 import org.eclipse.jgit.diff.RawTextComparator;
 import org.eclipse.jgit.dircache.DirCacheIterator;
-import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.Repository;
@@ -27,13 +22,11 @@ import org.eclipse.jgit.treewalk.FileTreeIterator;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.util.io.DisabledOutputStream;
 
-import com.jcabi.github.Repos;
 import com.jcabi.log.Logger;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.OutputStream;
-// import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -103,8 +96,6 @@ public class Experiments {
                     UtilFile.getAbsolutePath("dataset"), projectName, clientGitUrl);
 
             RevWalk rw = new RevWalk(repository);
-            // ObjectId head = repository.resolve(Constants.HEAD);
-            // ObjectId head = repository.resolve("4b00e834");
             ObjectId head = repository.resolve("934b20bd5f2ab87a201740a6cdc578704c3d08e6");
             RevCommit commit = rw.parseCommit(head);
             RevCommit parent = rw.parseCommit(commit.getParent(0).getId());
@@ -156,6 +147,8 @@ public class Experiments {
             List<Pair<String, String>> allDiffs = exp.getAllDiffsWithParentFromCommit(repository, commitToCheck);
             allDiffs.forEach(
                     p -> System.out.println(MessageFormat.format("File: {0}, \nDiff: {1}", p.getKey(), p.getValue())));
+
+            rw.close();
         }
     }
 
@@ -167,8 +160,6 @@ public class Experiments {
     }
 
     private void migrationIdentifier(String client, String projectName) {
-        // pseudocode+
-        // String client = "client git repo";
         String clientGitUrl = "";
 
         try {
@@ -177,17 +168,16 @@ public class Experiments {
                     UtilFile.getAbsolutePath("dataset"), projectName, clientGitUrl);
             RevWalk revWalk = service.createAllRevsWalk(repository, "master");
 
-            // ArrayList<RevCommit> clientCommits = new ArrayList<RevCommit>(); // get
-            // client commits here
             List<Change> BCs = new ArrayList<Change>();
             // TODO: parse BC files into List of Change or ChangeLike object
 
             Iterator<RevCommit> i = revWalk.iterator();
             while (i.hasNext()) {
                 RevCommit currentClientCommit = i.next();
+                // Granularity: commit time
                 int clientCommitTime = currentClientCommit.getCommitTime();
-                // ArrayList<File> javaFilesInCommit = new ArrayList<File>(); // get files in
-                // commit
+
+                // TODO: Probably not needed anymore
                 List<String> javaFileNamesInCommit = getJavaFileNamesInCommit(repository, currentClientCommit); // get
                                                                                                                 // files
                                                                                                                 // in
@@ -200,9 +190,6 @@ public class Experiments {
                         repository, currentClientCommit);
 
                 for (Pair<String, String> javaFileNameAndContent : javaFileNamesAndContentInCommit) {
-                    // int fileChangedTime = 0; // retrieve filechanged date, == commit date /
-                    // timestamp ?
-
                     ArrayList<Change> BcTimeFiltered = new ArrayList<Change>();
                     for (Change change : BCs) {
                         if (change.getRevCommit().getCommitTime() < clientCommitTime) {
@@ -211,11 +198,6 @@ public class Experiments {
                     }
 
                     String codeContent = javaFileNameAndContent.getValue();
-                    try {
-                        // codeContent = Files.readString(Paths.get(javaFile.getPath()));
-                    } catch (Exception ex) {
-                        Logger.error(ex, ex.getMessage());
-                    }
 
                     List<Change> importBCs = new ArrayList<Change>();
                     List<String> imports = ImportMatching.getImports(codeContent);
@@ -227,17 +209,6 @@ public class Experiments {
                         }
                     }
 
-                    // List<String> diffLines = new ArrayList(); // get the difflines from git diff
-                    // for (String diffLine : diffLines) {
-                    // for (Change change : BcTimeFiltered) {
-                    // String before = ""; // get diff before from diffLine
-                    // String after = ""; // get diff after from diffLine
-                    // if (change.getElement().contains(before) ||
-                    // change.getElement().contains(after)) {
-                    // // log migration
-                    // }
-                    // }
-                    // }
                     String diffsForFile = "";
                     for (Pair<String, String> diffs : diffsForFilesInCommit) {
                         if (diffs.getKey() == javaFileNameAndContent.getKey()) {
@@ -258,17 +229,10 @@ public class Experiments {
     }
 
     private List<Pair<String, String>> getAllDiffsWithParentFromCommit(Repository repository, RevCommit commit) {
-        // List<Pair<String, Pair<String, String>>> pathsWithDiffs = new
-        // ArrayList<Pair<String, Pair<String, String>>>();
         List<Pair<String, String>> pathsWithDiffs = new ArrayList<Pair<String, String>>();
 
         try {
             RevWalk rw = new RevWalk(repository);
-            // ObjectId head = repository.resolve(Constants.HEAD);
-            // ObjectId head = repository.resolve("4b00e834");
-            // ObjectId head =
-            // repository.resolve("934b20bd5f2ab87a201740a6cdc578704c3d08e6");
-            // RevCommit commit = rw.parseCommit(head);
             RevCommit parent = rw.parseCommit(commit.getParent(0).getId());
 
             OutputStream out = new ByteArrayOutputStream();
@@ -278,31 +242,6 @@ public class Experiments {
             df.setDiffComparator(RawTextComparator.DEFAULT);
             df.setDetectRenames(true);
             List<DiffEntry> diffs = df.scan(parent.getTree(), commit.getTree());
-            // for (DiffEntry diff : diffs) {
-            // String newPath = diff.getNewPath();
-
-            // for (Edit edit : df.toFileHeader(diff).toEditList()) {
-            // linesDeleted += edit.getEndA() - edit.getBeginA();
-            // linesAdded += edit.getEndB() - edit.getBeginB();
-            // }
-            // }
-
-            // for (DiffEntry diff : diffs) {
-            // try (DiffFormatter formatter = new DiffFormatter(new
-            // ByteArrayOutputStream())) {
-            // formatter.setRepository(repository);
-            // formatter.format(diff);
-            // System.out.println("Changed lines in: " + diff.getNewPath());
-            // formatter.toFileHeader(diff).toEditList().forEach(edit -> {
-            // for (int i = edit.getBeginA(); i < edit.getEndA(); i++) {
-            // System.out.println("- " + formatter.getOldString(i));
-            // }
-            // for (int i = edit.getBeginB(); i < edit.getEndB(); i++) {
-            // System.out.println("+ " + formatter.getNewString(i));
-            // }
-            // });
-            // }
-            // }
 
             for (DiffEntry diff : diffs) {
                 out = new ByteArrayOutputStream();
@@ -312,8 +251,6 @@ public class Experiments {
 
                     String fileDiff = out.toString();
 
-                    // System.out.println("Changed lines in: " + diff.getNewPath());
-                    // System.out.println(x);
                     pathsWithDiffs.add(Pair.of(diff.getNewPath(), fileDiff));
 
                     out.close();
@@ -334,11 +271,6 @@ public class Experiments {
 
         try {
             RevWalk rw = new RevWalk(repository);
-            // ObjectId head = repository.resolve(Constants.HEAD);
-            // ObjectId head = repository.resolve("4b00e834");
-            // ObjectId head =
-            // repository.resolve("934b20bd5f2ab87a201740a6cdc578704c3d08e6");
-            // RevCommit commit = rw.parseCommit(head);
             RevCommit parent = rw.parseCommit(commit.getParent(0).getId());
             DiffFormatter df = new DiffFormatter(DisabledOutputStream.INSTANCE);
             df.setRepository(repository);
@@ -368,9 +300,6 @@ public class Experiments {
         List<Pair<String, String>> filesContent = new ArrayList<>();
         RevWalk rw = new RevWalk(repository);
         try (TreeWalk tw = new TreeWalk(repository)) {
-            // RevCommit commitToCheck = rw.parseCommit(repository.resolve("HEAD"));
-            // RevCommit commitToCheck =
-            // rw.parseCommit(repository.resolve("934b20bd5f2ab87a201740a6cdc578704c3d08e6"));
             tw.addTree(commitToCheck.getTree());
             tw.addTree(new DirCacheIterator(repository.readDirCache()));
             tw.addTree(new FileTreeIterator(repository));
@@ -378,19 +307,9 @@ public class Experiments {
             while (tw.next()) {
                 if (tw.getPathString().contains(".java")) {
                     try {
-                        // System.out.printf(
-                        // "path: %s, Commit(mode/oid): %s/%s, Index(mode/oid): %s/%s,
-                        // Workingtree(mode/oid): %s/%s\n",
-                        // tw.getPathString(), tw.getFileMode(0), tw.getObjectId(0), tw.getFileMode(1),
-                        // tw.getObjectId(1),
-                        // tw.getFileMode(2), tw.getObjectId(2));
-
                         ObjectId objectId = tw.getObjectId(0);
                         ObjectLoader loader = repository.open(objectId);
                         String content = new String(loader.getBytes());
-
-                        // System.out.printf(
-                        // "Content: %s", content);
 
                         filesContent.add(Pair.of(tw.getPathString(), content));
                     } catch (Exception e) {
